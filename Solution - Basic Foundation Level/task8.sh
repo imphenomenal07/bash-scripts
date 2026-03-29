83fe9856155d6025805293100a851cc19962b800
Himanshu <169117364+imphenomenal07@users.noreply.github.com>
2026-03-29 13:38:27 +0530

    8. Write a script to find the largest of two numbers

---
+++ Solution - Basic Foundation Level/task8.sh
@@ -0,0 +1,12 @@
+# 8. Write a script to find the largest of two numbers
+
+read -p "Enter first number: " num1
+read -p "Enter second number: " num2
+
+if [ "$num1" -gt "$num2" ]; then
+    echo "$num1 is greater than $num2"
+elif [ "$num2" -gt "$num1" ]; then
+    echo "$num2 is greater than $num1"
+else
+    echo "Both numbers are equal"
+fi
